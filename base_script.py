fc576b78c089f98c333ededf86b0840b84153afc
Steven Lineses <stevenrlineses@gmail.com>
2020-06-28 15:32:35 -0600

    [KURO-13] Add code to remove all special characters from a string.

--- base_script.py
+++ base_script.py
@@ -1,11 +1,16 @@
+import re
+
 def stats():
     stats = {}
     with open("test.txt", "r") as text_file:
         for line in text_file:
             for word in split_line(line):
-                print(word, "\n")
+                print(remove_spec_chars(word), "\n")
 
 def split_line(line):
     return line.split(" ")
+
+def remove_spec_chars(string):
+    return re.sub('[^A-Za-z0-9]+', '', string)
 
 stats()
