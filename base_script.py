2cd171e42c1bb6eb3815119264f0327271080827
Steven Lineses <stevenrlineses@gmail.com>
2020-06-28 15:01:17 -0600

    [KURO-9] Add code to read each line and then split each line into separate strings

--- base_script.py
+++ base_script.py
@@ -2,6 +2,10 @@ def stats():
     stats = {}
     with open("test.txt", "r") as text_file:
         for line in text_file:
-            pass
+            for word in split_line(line):
+                print(word, "\n")
+
+def split_line(line):
+    return line.split(" ")
 
 stats()
