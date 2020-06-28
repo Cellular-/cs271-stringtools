f8998b9855d75f41649f60d8d662a98b34224a0a
Steven Lineses <stevenrlineses@gmail.com>
2020-06-28 17:21:34 -0600

    [KURO-14] Add code to read and generate stats for all .txt files in the base directory.

--- base_script.py
+++ base_script.py
@@ -1,4 +1,4 @@
-import re
+import re, glob, os
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 frequencies = {}
@@ -7,23 +7,28 @@ def setup():
     for letter in alphabet:
         frequencies[letter] = 0
 
-def stats():
-    setup()
-    with open("test.txt", "r") as text_file:
-        for line in text_file:
-            for word in split_line(line):
-                fc = first_char(remove_spec_chars(word))
-                if fc in alphabet:
-                    frequencies[fc] += 1
+    return glob.glob("./*.txt")
 
-    write_frequencies()
+def stats():
+    filenames = setup()
+
+    for filename in filenames:
+        with open(filename, "r") as text_file:
+            for line in text_file:
+                for word in split_line(line):
+                    fc = first_char(remove_spec_chars(word))
+                    if fc in alphabet:
+                        frequencies[fc] += 1
+
+        write_frequencies(filename)
 
 def print_frequencies():
     for k, v in frequencies.items():
         print(k, v, "\n")
 
-def write_frequencies():
-    with open("output/stats.txt", "w") as file:
+def write_frequencies(filename):
+    filename = filename.replace(".txt", "")
+    with open(f"output/{filename}-stats.txt", "w") as file:
         for k, v in frequencies.items():
             file.write(f"{k} - {v}\n")
 
